47da3753b2760cedfd8fd9a751a3d1de7038929c
Shivendra <shivendramaury@gmail.com>
2026-08-04 18:48:14 +0530

    Ready for deployment

--- app.js
+++ app.js
@@ -48,14 +48,17 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 // ==================== Session ====================
 
+app.set("trust proxy", 1);
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
     },
 };
 
